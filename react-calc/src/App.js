464c9a11bc6a9bc6daa659337780927806c85cd6
sigmatconsultancy <sigmatconsultancy@gmail.com>
2023-02-15 18:41:15 +0530

    html

---
+++ react-calc/src/App.js
@@ -0,0 +1,46 @@
+const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate'
+}
+
+function reducer(state, {type,payload}) {
+  switch(type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand: ${currentOperand || "" }${payload.digit},
+      }
+
+    
+  }
+}
+
+function App() {
+  const [{currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer,{})
+dispatch({ type:ACTIONS.ADD_DIGIT, payload: { digit: 1}})
+    return (
+   <div className="calculater-grid">
+    <div className="Output">
+      <div className="previous-operand"></div>
+      <div className="current-operand"></div>
+   </div>
+   <button className="span-two">AC</button>
+   <button>DEL</button>
+   <button>/</button>
+   <button>1</button>
+   <button>2</button>
+   <button>3</button>
+   <button>4</button>
+   <button>5</button>
+   <button>6</button>
+   <button>7</button>
+   <button>8</button>
+   <button>9</button>
+   <button>-</button>
+   <button>.</button>
+   <button>0</button>
+   <button className="span-two">=</button>
+   </div>
